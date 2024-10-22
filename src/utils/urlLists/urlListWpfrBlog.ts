1023b4ccffa9f856d4258f68d5479a8eab472b5e
cavaglieridomenico@gmail.com <cavaglieridomenico@gmail.com>
2024-10-22 05:52:44 +0200

    Add new url lists

---
+++ src/utils/urlLists/urlListWpfrBlog.ts
@@ -0,0 +1,16 @@
+export const urlListWpfrBlog = [
+  "https://www.whirlpool.fr/conseils-et-astuces/cuisson/nettoyer-hotte-cuisine",
+  "https://www.whirlpool.fr/conseils-et-astuces/cuisson/nettoyer-son-four",
+  "https://www.whirlpool.fr/conseils-et-astuces/cuisson/symboles-four",
+  "https://www.whirlpool.fr/conseils-et-astuces/lavage/nettoyer-condenseur-seche-linge",
+  "https://www.whirlpool.fr/conseils-et-astuces/lavage/nettoyer-sa-machine-a-laver",
+  "https://www.whirlpool.fr/conseils-et-astuces/lavage/programmes-et-cycles-lavage",
+  "https://www.whirlpool.fr/conseils-et-astuces/lavage/quelle-est-la-difference-entre-integrable-et-encastrable",
+  "https://www.whirlpool.fr/conseils-et-astuces/lave-vaisselle/comment-reinitialiser-un-lave-vaisselle-whirlpool",
+  "https://www.whirlpool.fr/conseils-et-astuces/lave-vaisselle/fonctionnement-lave-vaisselle",
+  "https://www.whirlpool.fr/conseils-et-astuces/lave-vaisselle/fuite-lave-vaisselle",
+  "https://www.whirlpool.fr/conseils-et-astuces/lave-vaisselle/nettoyer-un-lave-vaisselle",
+  "https://www.whirlpool.fr/conseils-et-astuces/lave-vaisselle/odeurs-lave-vaisselle",
+  "https://www.whirlpool.fr/conseils-et-astuces/lave-vaisselle/sel-et-liquide-de-rincage",
+  "https://www.whirlpool.fr/conseils-et-astuces/lave-vaisselle/symboles-lave-vaisselle",
+];
