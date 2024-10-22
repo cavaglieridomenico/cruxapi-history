d556937a8dd2bc2e81ece0ba9bc7231cbd645f68
cavaglieridomenico@gmail.com <cavaglieridomenico@gmail.com>
2024-10-22 05:52:44 +0200

    Add new url lists

---
+++ src/utils/urlLists/urlListHomepages.ts
@@ -0,0 +1,8 @@
+export const urlListHomepages = [
+  "https://www.whirlpool.it",
+  "https://www.whirlpool.pl",
+  "https://www.whirlpool.fr",
+  "https://www.bauknecht.de",
+  "https://www.hotpoint.it",
+  "https://www.hotpoint.co.uk",
+];
